f696c623d32c16c08bd7c99b25a50417abb45641
Thomas Selwyn <thomas@selwyn.tech>
2025-10-05 21:20:06 -0400

    Analysis work

--- analysis_program/analysis.py
+++ analysis_program/analysis.py
@@ -1,10 +1,8 @@
 import os
 import subprocess
-from time import sleep
 
 trace_files = "../input_files"
 executable_path = "../bin/interrupts"
-default_args = "vector_table.txt device_table.txt"
 
 def main():
     print("Analysis")
@@ -12,15 +10,14 @@ def main():
     for file in os.listdir(trace_files):
         filepath = os.path.join(trace_files, file)
         execute_sim(15, 20, filepath)
-        print(file)
-        sleep(50000)
 
 
 def execute_sim(context_save_time: int, isr_activity_time: int, trace_file: str):
     # Executing one simulation
-    result = subprocess.run([executable_path, "../input_files/trace_1.txt", default_args],
+    result = subprocess.run([executable_path, "../input_files/trace_1.txt", "../vector_table.txt", "../device_table.txt", str(context_save_time), str(isr_activity_time)],
                             capture_output=True)
     print(result)
+
 
 if __name__ == "__main__":
     main()
