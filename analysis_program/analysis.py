c6434cc2a352bc972a106fafa1ca3aaf24e1f467
Thomas Selwyn <thomas@selwyn.tech>
2025-10-05 22:24:31 -0400

    Add output files to repo

--- analysis_program/analysis.py
+++ analysis_program/analysis.py
@@ -10,11 +10,11 @@ executable_path = "../bin/interrupts"
 context_save_time = [10, 20, 30]
 isr_activity_time = [40, 70, 110, 130, 160, 200]
 
-
 def main():
     print("Devils Trace Analysis")
 
     # Assert Linux Dominance
+    # Python on Windows was being weird running the ./bin/interrupts file, so I just scraped windows support
     if platform.system() != "Linux":
         print("Linux is better.")
         exit()
@@ -27,19 +27,19 @@ def main():
 
     # Read through input files
     for file in os.listdir(trace_files):
-        filepath = os.path.join(trace_files, file)
+        filepath = Path(trace_files).joinpath(file)
 
         for save_time in context_save_time:
             for isr_time in isr_activity_time:
                 execute_sim(filepath, save_time, isr_time)
 
-def execute_sim(trace_filepath: str, save_time: int, isr_time: int):
+def execute_sim(trace_filepath: Path, save_time: int, isr_time: int):
     # Executing one simulation
     result = subprocess.run(
-        [executable_path, trace_filepath, "../vector_table.txt", "../device_table.txt", str(save_time), str(isr_time)],
+        [executable_path, str(trace_filepath), "../vector_table.txt", "../device_table.txt", str(save_time), str(isr_time)],
         capture_output=True)
 
-    trace_file = Path(trace_filepath).stem + "_" + str(save_time) + "_" + str(isr_time)
+    trace_file = trace_filepath.stem + "_" + str(save_time) + "_" + str(isr_time) + ".csv"
     sim_output = Path(output_files).joinpath(trace_file)
 
     try:
